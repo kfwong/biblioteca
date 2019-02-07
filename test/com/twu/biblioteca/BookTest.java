e4d06acbaf85dc67566f4a2dc869768d8dd8df97
WONG KANG FEI <kangfei.wong@thoughtworks.com>
2019-02-07 10:35:37 +0800

    implement and pass book getter tests.

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -4,12 +4,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 public class BookTest {
+    Book book = new Book("title", "author", 2018);
 
     @Test
     public void should_instantiate_valid_book_instance(){
-        Book book = new Book("title", "author", 2018);
 
         assertNotNull(book);
 
+    }
+
+    @Test
+    public void should_be_able_to_read_all_fields(){
+
+        assertEquals("title", book.getTitle());
+        assertEquals("author", book.getAuthor());
+        assertEquals(2018, book.getYear());
     }
 }
