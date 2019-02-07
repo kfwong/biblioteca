e16779dd92df70a0e902527ad0e01a7c00368e49
WONG KANG FEI <kangfei.wong@thoughtworks.com>
2019-02-07 10:27:50 +0800

    Implement and Pass list books test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,6 +2,8 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    private final String[] books = {"Harry Potter and the Deathly Hallows", "Fifty Shades of Grey"};
+
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
 
@@ -10,5 +12,9 @@ public class BibliotecaApp {
 
     public void displayWelcomeMessage(){
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    public void displayListOfBooks(){
+        System.out.println(String.join("\n", books));
     }
 }
